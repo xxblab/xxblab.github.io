64c84ca8ac4ef73c8cd50066088e6e9e5db2c83c
xxblab <106507196+xxblab@users.noreply.github.com>
2024-03-14 06:29:37 +0800

    Add files via upload

---
+++ bo_bar.js
@@ -0,0 +1,10 @@
+document.writeln("<!DOCTYPE html>");
+document.writeln("<html>");
+document.writeln("<head>");
+document.writeln("	<meta charset=\"utf-8\">");
+document.writeln("	<link rel=\"stylesheet\" type=\"text/css\" href=\"18css.css\">");
+document.writeln("</head>");
+document.writeln("<body>");
+document.writeln("<div class=\"bo_bar\"><img src=\"https://13511974.s21i.faiusr.com/4/10/ABUIABAEGAAg-My8rwYozLjCmAIw6gI4owE.png\" height=\"50px\"><p>|&nbsp&nbsp&nbsp© 2020-2022&nbsp&nbsp&nbsp|&nbsp&nbsp&nbsp网站设计与维护：春潮信息&nbsp&nbsp&nbsp|</p></div>");
+document.writeln("</body>");
+document.writeln("</html>");
---
+++ to_bar.js
@@ -0,0 +1,38 @@
+document.writeln("<!DOCTYPE html>");
+document.writeln("<html>");
+document.writeln("<head>");
+document.writeln("	<meta charset=\"utf-8\">");
+document.writeln("	<link rel=\"stylesheet\" type=\"text/css\" href=\"18css.css\">");
+document.writeln("  <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->");
+document.writeln("    <link rel=\"stylesheet\" href=\"https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css\">");
+document.writeln("    <script src=\"https://cdn.staticfile.net/jquery/2.1.1/jquery.min.js\"></script>");
+document.writeln("    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->");
+document.writeln("    <script src=\"https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js\"></script>");
+document.writeln("</head>");
+document.writeln("<body>");
+
+document.writeln("<div class=\"logo\"><img src=\"https://13511974.s21i.faiusr.com/4/10/ABUIABAEGAAg09a8rwYojqf82QIwtwI4tQE.png\" height=\"110px\">");
+document.writeln("<div class=\"input-group input-top-gp\">");
+document.writeln("					<input type=\"text\" class=\"form-control\" placeholder=\"请输入要搜索的内容\">");
+document.writeln("					<span class=\"input-group-btn\">");
+document.writeln("						<a class=\"btn  btn-seas\" type=\"button\">");
+document.writeln("							 &nbsp&nbsp");
+document.writeln("						</a>");
+document.writeln("					</span>");
+document.writeln("				</div></div>");
+
+
+document.writeln("<ul>");
+document.writeln("  <li><a href=\"/\" style=\"margin-left:14.2vw;\">首页</a></li>");
+document.writeln("  <li><a href=\"/h-about.htm\">关于春潮</a></li>");
+document.writeln("  <li><a href=\"/h-tg.htm\">专栏投稿</a></li>");
+document.writeln("  <li><a href=\"/h-jb.htm\">侵权内容举报</a></li>");
+document.writeln("  <div class=\"dropdowna\">");
+document.writeln("    <a class=\"dropbtna\">信息公开</a>");
+document.writeln("    <div class=\"dropdown-contenta\">");
+document.writeln("      <a href=\"/h-news-49.htm\">2024年度计划</a>");
+document.writeln("    </div>");
+document.writeln("  </div>");
+document.writeln("</ul>");
+document.writeln("</body>");
+document.writeln("</html>");
